bd4f1af228bd775eca1ddc34e90ef69f0572c24a
Paul Crowley <paulcrowley@google.com>
2018-09-02 20:31:50 -0700

    Move parsing into its own file.

---
+++ parse.py
@@ -0,0 +1,30 @@
+import lark
+
+import lterm
+import paths
+
+grammar = (paths.top / "grammar.lark").read_text()
+expr_parser = lark.Lark(grammar, start='expr', parser='lalr')
+directives_parser = lark.Lark(grammar, start='directives', parser='lalr')
+
+@lark.v_args(inline=True)
+class Transformer(lark.Transformer):
+    def var(self, v):
+        return lterm.Var(v.value)
+
+    def apply(self, a, b):
+        return lterm.Apply(a, b)
+
+    def mlambda(self, vars, e):
+        res = e
+        for v in reversed(vars.children):
+            res = lterm.Lambda(v.value, res)
+        return res
+
+transformer = Transformer()
+
+def parse_expr(expr):
+    return transformer.transform(expr_parser.parse(expr))
+
+def parse_directives(expr):
+    return transformer.transform(directives_parser.parse(expr))
---
+++ paths.py
@@ -0,0 +1,5 @@
+import pathlib
+
+_file = pathlib.Path(__file__).resolve()
+
+top = _file.parent
