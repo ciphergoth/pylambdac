2d6fe5ae13a9c9be746ddb3782aaa9a85b6c3896
Paul Crowley <paulcrowley@google.com>
2018-09-04 08:52:29 -0700

    Simplify reduction check the same way.

--- test_reduce.py
+++ test_reduce.py
@@ -2,30 +2,19 @@ import lterm
 import parse
 import tdata
 
-def test_reduce():
-    for example in testfile.read():
-        parsed = [parse.parse_expr(e) for e in example]
-        state = parsed[0]
-        print(f"Started with: {state}")
-        state = state.reduce_once({})
-        for next in parsed[1:]:
-            print(f"Expected: {next}")
-            print(f"Got: {state}")
-            assert next.equiv(state)
+class Check(tdata.CheckConsistent):
+    def answer(self, example):
+        new  = []
+        state = parse.parse_expr(example)
+        while state is not None:
+            new.append(str(state))
             state = state.reduce_once({})
-        assert state is None
+        return new
 
-class Tdata(tdata.Tdata):
-    def fixup(self, examples):
-        for example in examples:
-            new  = []
-            state = parse.parse_expr(example[0])
-            while state is not None:
-                new.append(str(state))
-                state = state.reduce_once({})
-            yield new
+testfile = Check("test_reduce")
 
-testfile = Tdata("test_reduce")
+def test_reduce():
+    testfile.check_all()
 
 if __name__ == '__main__':
     testfile.main()
