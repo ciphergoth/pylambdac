7c1dd8e552c7697140c7b5d4217e0a57a5fe0c58
Paul Crowley <paulcrowley@google.com>
2018-09-04 08:56:10 -0700

    Simplify parse check the same way.

--- test_parse.py
+++ test_parse.py
@@ -2,20 +2,14 @@ import lterm
 import parse
 import tdata
 
+class Check(tdata.CheckConsistent):
+    def answer(self, s):
+        return parse.parse_expr(s).prefixcode(debruijn=False)
+
+testfile = Check("test_parse")
+
 def test_parse():
-    for example in testfile.read():
-        s = example[0]
-        expr = parse.parse_expr(s)
-        assert example[1] == expr.prefixcode(debruijn=False)
-
-class Tdata(tdata.Tdata):
-    def fixup(self, examples):
-        for example in examples:
-            s = example[0]
-            expr = parse.parse_expr(s)
-            yield [s, expr.prefixcode(debruijn=False)]
-
-testfile = Tdata("test_parse")
+    testfile.check_all()
 
 if __name__ == '__main__':
     testfile.main()
