89aa79254ad6f373d2bef06a21882ba62e098532
Paul Crowley <paulcrowley@google.com>
2020-02-20 07:28:13 -0800

    Alternative diagrams

--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ class Lexp:
 class Var(Lexp):
     def __init__(self, d):
         self._d = d
-        self.size = [1, 0]
+        self.size = (1, 0, 0, 0)
 
     def draw(self, grid, wo, ho, ll):
         for i in range(ll[-(self._d + 1)] * 4 + 2, ho * 4 + 3):
@@ -15,8 +15,8 @@ class Var(Lexp):
 class Lambda(Lexp):
     def __init__(self, e):
         self._e = e
-        w, h = e.size
-        self.size = [w, h + 1]
+        s = e.size
+        self.size = (s[0], s[1] + 1, s[2], s[3])
 
     def draw(self, grid, wo, ho, ll):
         for i in range(wo * 4 + 1, (wo + self.size[0] -1) * 4 + 4):
@@ -29,18 +29,20 @@ class Apply(Lexp):
     def __init__(self, l, r):
         self._l = l
         self._r = r
-        wl, hl = l.size
-        wr, hr = r.size
-        self.size = [wl + wr, max(hl, hr) + 1]
+        ls = l.size
+        rs = r.size
+        self.size = (ls[0] + rs[0], max(ls[1], rs[1]) + 1, ls[3], ls[0] + rs[2])
 
     def draw(self, grid, wo, ho, ll):
         self._l.draw(grid, wo, ho, ll)
-        for i in range((ho + self._l.size[1] -1) * 4 + 2, (ho + self.size[1] -1) * 4 + 3):
-            grid[i][wo * 4 + 2] = "#"
         self._r.draw(grid, wo + self._l.size[0], ho, ll)
+        l = (wo + self.size[2]) * 4 + 2
+        r = (wo + self.size[3]) * 4 + 2
+        for i in range((ho + self._l.size[1] -1) * 4 + 2, (ho + self.size[1] -1) * 4 + 3):
+            grid[i][l] = "#"
         for i in range((ho + self._r.size[1] -1) * 4 + 2, (ho + self.size[1] -1) * 4 + 3):
-            grid[i][(wo + self._l.size[0]) * 4 + 2] = "#"
-        for i in range(wo * 4 + 2, (wo + self._l.size[0]) * 4 + 3):
+            grid[i][r] = "#"
+        for i in range(l, r + 1):
             grid[(ho + self.size[1] -1) * 4 + 2][i] = "#"
 
 
@@ -51,7 +53,8 @@ def gex():
     return l(a(l(a(v(0), v(0))), l(a(v(1), a(v(0), v(0))))))
 
 ex = gex()
-w, h = ex.size
+w = ex.size[0]
+h = ex.size[1]
 
 grid = [[" " for _ in range(w * 4 + 1)] for _ in range(h * 4 + 1)]
 ex.draw(grid, 0, 0, [])
