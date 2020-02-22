c264923baf9a0053324494007754732b75a2f9f4
Paul Crowley <paulcrowley@google.com>
2020-02-22 15:59:37 -0800

    draw bugfixes

--- draw.py
+++ draw.py
@@ -19,7 +19,7 @@ class Grid:
     def yield_lines(self, rstep):
         yield [0 for _ in range(self.w * 4 + 1)]
         for i, l in enumerate(self.grid):
-            for _ in range(1 + (rstep -1) * (i % 2)):
+            for _ in range(1 + (rstep -2) * (i % 2)):
                 yield l
         yield [0 for _ in range(self.w * 4 + 1)]
 
@@ -28,15 +28,19 @@ class Grid:
             print("".join(" #"[x] for x in l))
 
     def writepbm(self, outfile, pbmscale=10):
-        with open(outfile, "w") as f:
+        rstep = 4
+        with outfile.open("w") as f:
             f.write(f"P1\n")
-            f.write(f"{pbmscale * len(self.grid[0])} {pbmscale * len(self.grid)}\n")
-            for l in self.yield_lines(4):
+            f.write(f"{pbmscale * (self.w * 4 + 1)} {pbmscale * (self.h * rstep -1)}\n")
+            for l in self.yield_lines(rstep):
                 for _ in range(pbmscale):
                     f.write(" ".join("01"[x] for x in l for _ in range(pbmscale)))
                     f.write("\n")
 
 def draw_expr(expr):
-    grid = Grid(expr.draw_dims[0], expr.draw_dims[1])
+    h, w = expr.draw_dims[0:2]
+    if w == 1:
+        h += 1
+    grid = Grid(h, w)
     expr.draw(grid, 0, 0, {})
     return grid
