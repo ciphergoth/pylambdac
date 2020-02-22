3f5ac92f9315cc37a0cd602c355bf947c047b9a5
Paul Crowley <paulcrowley@google.com>
2020-02-22 15:59:37 -0800

    Crude PBM output tool

--- draw.py
+++ draw.py
@@ -1,22 +1,32 @@
 class Grid:
+    _rstep = 4
     def __init__(self, h, w):
-        self.grid = [[" " for _ in range(w * 4 + 1)] for _ in range(h * 3)]
+        self.grid = [[0 for _ in range(w * 4 + 1)] for _ in range((h -1) * self._rstep + 3)]
 
     def drawh(self, r, cstart, cend):
         for i in range(cstart * 4 + 2, cend * 4 + 3):
-            self.grid[r * 3 + 1][i] = "#"
+            self.grid[r * self._rstep + 1][i] = 1
 
     def drawl(self, r, cstart, cend):
          for i in range(cstart * 4 + 1, cend * 4 + 4):
-            self.grid[r * 3 + 1][i] = "#"
+            self.grid[r * self._rstep + 1][i] = 1
 
     def drawv(self, rstart, rend, c):
-        for i in range(rstart * 3 + 1, rend * 3 + 2):
-            self.grid[i][c * 4 + 2] = "#"
+        for i in range(rstart * self._rstep + 1, rend * self._rstep + 2):
+            self.grid[i][c * 4 + 2] = 1
 
     def print(self):
         for l in self.grid:
-            print("".join(l))
+            print("".join(" #"[x] for x in l))
+
+    def writepbm(self, outfile, pbmscale=10):
+        with open(outfile, "w") as f:
+            f.write(f"P1\n")
+            f.write(f"{pbmscale * len(self.grid[0])} {pbmscale * len(self.grid)}\n")
+            for l in self.grid:
+                for _ in range(pbmscale):
+                    f.write(" ".join("01"[x] for x in l for _ in range(pbmscale)))
+                    f.write("\n")
 
 def draw_expr(expr):
     grid = Grid(expr.draw_dims[0], expr.draw_dims[1])
